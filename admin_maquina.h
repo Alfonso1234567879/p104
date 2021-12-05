1e523013ad3d012ea7ecff6276d8e6846d6363e9
Alfonso1234567879 <91120187+Alfonso1234567879@users.noreply.github.com>
2021-12-05 18:39:52 +0100

    Update admin_maquina.h

--- admin_maquina.h
+++ admin_maquina.h
@@ -1,2 +1,18 @@
 //clase admin-maquina
+#ifndef ADMINMAQUINA
+#define ADMINMAQUINA
+#include<cstdlib>
+#include<string>
+#include"admin.h"
+#include"maquina.h"
+using namespace std;
+class AdminMaq:public Admin{
 
+	public:
+		AdminMaq(string idadmin,string dni,string contrasena):Admin(idadmin,dni,contrasena){};
+		bool addMaq(Maquina a);
+		int moodMaq(Maquina a);
+		void deleteMaq(Maquina a);
+		
+};
+#endif
